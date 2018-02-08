2d8670e0b94f399568025a5e689fc76c1c8b67e5
Johannes Breytenbach <websalesabc@gmail.com>
2018-02-09 07:51:33 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React, {Components} from 'react';
+import factory from '../ethereum/factory';
+
+class CampaignIndex extends Component{
+  async componentDidMount() {
+  const campaigns =  await factory.methods.getDeployedCampaigns().call();
+
+  console.log(campaigns);
+
+  }
+
+  render() {
+    return <div>Campaign Index!</div>
+  }
+}
+
+export default CampaignIndex;
