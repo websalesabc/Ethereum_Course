f8b867a3344ce96c053c52850df12ea54f62de14
Johannes Breytenbach <websalesabc@gmail.com>
2018-02-09 12:36:57 +1000

    Delete factory.js

--- factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
-
-const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
-  '0x967b879715432b851aFb5525982E9601a3C90573'
-);
-
-export default instance;
